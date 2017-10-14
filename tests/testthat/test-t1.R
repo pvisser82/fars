be4f250d8777624dff71064fed14b6420d69426e
Paul Visser <visser.paul@gmail.com>
2017-10-14 23:54:36 +0200

    updated DESCRIPTION file

--- tests/testthat/test-t1.R
+++ tests/testthat/test-t1.R
@@ -1,11 +1,8 @@
-context("Test the basic functionality of the package")
+context("Test the fars package using testthat")
 
 library(dplyr)
 library(maps)
 
-test_that("fars_summarize_years() works correctly", {
+test_that("fars_summarize_years() works correctly for a year range", {
   expect_is(fars_summarize_years(2013:2015), "tbl_df")
-  expect_is(fars_summarize_years(list(2013, 2014)), "tbl_df")
-  expect_equal(names(fars_summarize_years(2013:2015)), c("MONTH", 2013:2015))
-  expect_error(fars_summarize_years(2016))
 })
