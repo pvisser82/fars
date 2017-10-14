0caaddfee78064518f9f1baa55197bae6282ecff
Paul Visser <visser.paul@gmail.com>
2017-10-14 23:35:59 +0200

    updated testthat

--- tests/testthat/test-t1.R
+++ tests/testthat/test-t1.R
@@ -3,8 +3,6 @@ context("Test the basic functionality of the package")
 library(dplyr)
 library(maps)
 
-setwd(system.file("./data", package = "fars"))
-
 test_that("fars_summarize_years() works correctly", {
   expect_is(fars_summarize_years(2013:2015), "tbl_df")
   expect_is(fars_summarize_years(list(2013, 2014)), "tbl_df")
